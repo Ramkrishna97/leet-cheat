99e6503e065c3e189fcd0a07e8440bfaa734fced
Ramkrishna97 <ramkrishnagoswami10@gmail.com>
2026-01-07 20:18:49 +0530

    leetcode problem 217 - contains duplicate

--- ContainnsDuplicate.java
+++ ContainnsDuplicate.java
@@ -16,29 +16,31 @@ Output: false
 public class ContainnsDuplicate {
     public static void main(String[] args) {        
         System.out.println("Class created successfully");
-        Solution.test();
+        System.out.println(test());
     }
-}
-
-class Solution{
+    
     public static boolean test(){
         int[]nums={1,2,3,1};
-
+            // brute force approach : O(n^2) time complexity, easy to understand, for first loop it takes one input
+            // with second loop it check all other element for the first element if it is duplicate or not. 
         // for(int i=0;i<nums.length;i++){
-            for(int j=i+1;j<nums.length;j++){
-                if(nums[i]==nums[j]){
-                    return true;
-                }
-            }
+            // for(int j=i+1;j<nums.length;j++){
+            //     if(nums[i]==nums[j]){
+            //         return true;
+            //     }
+            // }
         
+            //this method first sorts, so that duplicates elements are one after another
+            //then it checks for duplicates, time complexity O(n log n) due to sorting
+        // java.util.Arrays.sort(nums);
+        // for(int i=0;i<nums.length-1;i++){
+        //     if(nums[i]==nums[i+1]){
+        //     }
+        // }
 
-        java.util.Arrays.sort(nums);
-        for(int i=0;i<nums.length-1;i++){
-            if(nums[i]==nums[i+1]){
-            }
-        }
-
-        //best solution 
+        //best solution : time complexity O(n), space complexity O(n)
+        //it tries to add elements to a set, if already present it returns true.
+        //(it passes because only one duplicate element is expected)
         java.util.HashSet<Integer> set = new java.util.HashSet<>();
           for (int num : nums) {
             if (!set.add(num)) {
