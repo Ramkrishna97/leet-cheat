c93e1caefbb065bf1e8e0d44909626883738ff23
Ramkrishna97 <ramkrishnagoswami10@gmail.com>
2026-01-17 13:21:56 +0530

    added few comments

--- GroupAnagram.java
+++ GroupAnagram.java
@@ -1,5 +1,29 @@
 import java.util.*;
+/*
+49. Group Anagrams(Medium)
+Given an array of strings strs, group the
+together. You can return the answer in any order.
+Example 1:
+Input: strs = ["eat","tea","tan","ate","nat","bat"]
+Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
+Explanation:
+    There is no string in strs that can be rearranged to form "bat".
+    The strings "nat" and "tan" are anagrams as they can be rearranged to form each other.
+    The strings "ate", "eat", and "tea" are anagrams as they can be rearranged to form each other.
 
+Example 2:
+Input: strs = [""]
+Output: [[""]]
+
+Example 3:
+Input: strs = ["a"]
+Output: [["a"]]
+
+Constraints:
+    1 <= strs.length <= 104
+    0 <= strs[i].length <= 100
+    strs[i] consists of lowercase English letters.
+*/
 public class GroupAnagram {
     public static void main(String[] args) {
         String[] strs={"eat","tea","tan","ate","nat","bat"};
@@ -10,7 +34,9 @@ public class GroupAnagram {
 }
 class Solution {
     public static List<List<String>> groupAnagrams(String[] strs) {
-        //brute force: try to add elements in  a set, if exists then return combined list.  xx-failes to group
+        //brute force: try to add elements in  a set, if exists then return combined list.
+        //   xx-failes to group 
+        // time complexity : O(n^2)
 
         int length=strs.length;
         List<List<String>> result=null;
@@ -18,6 +44,7 @@ class Solution {
         Map<String,List<String>> map=new HashMap<>();
         //Optimum approach: for each element, sort as a char array, make it as a key for INDEXing, 
         // if key exists then add value to  list, otherwise add key and make new value as a list.
+        //time complexity : O(n*klogk) , n=number of strings, k= max length of a string.
 
         if(length==0){
             return new ArrayList<>();
