116d82b52ed52c77c9575681df08d1ca13b3d99d
Ramkrishna97 <ramkrishnagoswami10@gmail.com>
2026-02-25 23:21:03 +0530

    167. Two Sum - sorted Array

--- TwoSum2.java
+++ TwoSum2.java
@@ -1,5 +1,4 @@
 
-
 /*
 167. Two Sum - sorted Array [EASY ]
 Given a 1-indexed array of integers numbers that is already sorted in non-decreasing order, find two numbers such that they add up to a specific target number. 
